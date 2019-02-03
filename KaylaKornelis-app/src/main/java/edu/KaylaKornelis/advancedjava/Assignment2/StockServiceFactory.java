af09b1b0f8e7b9d483a0859ed075f008736bf3ef
Kayla Kornelis <kayla.kornelis@gmail.com>
2019-02-02 23:51:00 -0600

    assignment 2 - draft 2 - added javadoc comments and junit tests for all public methods. removed stockticker and stockticker test classes

--- KaylaKornelis-app/src/main/java/edu/KaylaKornelis/advancedjava/Assignment2/StockServiceFactory.java
+++ KaylaKornelis-app/src/main/java/edu/KaylaKornelis/advancedjava/Assignment2/StockServiceFactory.java
@@ -1,18 +1,16 @@
-
 package edu.KaylaKornelis.advancedjava.Assignment2;
 
 /**
- *
- * @author Kornelis
+ *  This class serves as a Factory for the StockService interface and classes that implement the StockService interface for getting stock data.
+ * Allows creation of new objects without exposing the creation logic to the client.
  */
+
 public class StockServiceFactory {
     
-    public StockService getStockService(StockService stockServiceType){
-        if (stockServiceType == null){
-            return null;
-        } else {
-            return new BasicStockService();
-        }
-        
+    public StockService getStockService(){
+        /**
+         * Create and return a new instance of BasicStockService.
+         */
+        return new BasicStockService();   
     }
 }
