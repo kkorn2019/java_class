6194b46159c03c6f31a308b6442dc4929412feaf
Kayla Kornelis <kayla.kornelis@gmail.com>
2019-02-03 23:48:15 -0600

    revisions to assignment one - reworked class structure and added javadoc throughout

--- KaylaKornelis-app/src/main/java/edu/KaylaKornelis/advancedjava/Assignment1/Book.java
+++ KaylaKornelis-app/src/main/java/edu/KaylaKornelis/advancedjava/Assignment1/Book.java
@@ -1,47 +1,77 @@
 package edu.KaylaKornelis.advancedjava.Assignment1;
 
-public class Book{
+/**
+ * This class contains information about a book.
+ * 
+ * It contains methods to set and retrieve the title, author, 
+ * and genre attributes of a give book. 
+ */
+public class Book {
 
     //Attributes of a Book
-    private String title;
-    private String author;
-    private String genre;
+    BookTitle title = new BookTitle(new String());
+    BookAuthor author = new BookAuthor(new String());
+    BookGenre genre = new BookGenre(new String());
 
     /**
      * Create a new Book using the following attributes
      *
      * @param title - the title of the chosen book
-     * @param author - the name of the Author who wrote the book
-     * @param genre - the genre the book is categorized by
+     * @param author - the Author who wrote the book
+     * @param genre - the genre by which the book is categorized
      *
      */
-    public Book(String title, String author, String genre){
+    public Book(BookTitle title, BookAuthor author, BookGenre genre){
         this.title = title;
         this.author = author;
         this.genre = genre;
     }
-
-    public void setTitle(String title){
+    
+    /**
+     * Set the title for the book
+     * @param title the name of the book
+    */
+    public void setTitle(BookTitle title){
         this.title = title;
     }
-
-    public void setAuthor(String author){
+    
+    /**
+     * Set the value of author to the value given 
+     * @param author the desired name of the author of the book
+     */
+    public void setAuthor(BookAuthor author){
         this.author = author;
     }
-
-    public void setGenre(String genre){
+    
+    /**
+     * Set the value of genre to the value given 
+     * @param genre the desired category for the book
+     */
+    public void setGenre(BookGenre genre){
         this.genre = genre;
     }
-
+    
+     /**
+     * retrieve the name of the book
+     * @return a String
+    */
     public String getTitle(){
-        return title;
+        return title.getTitle();
     }
-
+    
+    /**
+     * retrieve the name of the author
+     * @return a String
+    */
     public String getAuthor(){
-        return author;
+        return author.getAuthor();
     }
-
+    
+    /**
+     * retrieve the genre by which the book is categorized
+     * @return a String
+    */
     public String getGenre(){
-        return genre;
+        return genre.getGenre();
     }
 }
