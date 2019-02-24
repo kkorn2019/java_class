e5084178ef0f1855b35320ce337db92e935a85e7
Kayla Kornelis <kayla.kornelis@gmail.com>
2019-02-24 17:59:01 -0600

    adding master files back in

---
+++ src/main/java/edu/KaylaKornelis/advancedjava/AdvancedJavaAssignment4/service/StockService.java
@@ -0,0 +1,55 @@
+package edu.KaylaKornelis.advancedjava.AdvancedJavaAssignment4.service;
+
+import edu.KaylaKornelis.advancedjava.AdvancedJavaAssignment4.model.Interval;
+import edu.KaylaKornelis.advancedjava.AdvancedJavaAssignment4.model.StockQuote;
+import java.util.Calendar;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import org.apache.http.annotation.Immutable;
+
+/**
+ *  This interface describes an API for getting stock data.
+ */
+@Immutable
+public interface StockService {
+
+    /**
+     * Return the current price for a share of stock
+     * for the given symbol
+     *
+     * @param symbol the stock symbol of the company you want a quote for.
+     *      e.g. APPL for APPLE
+     *
+     * @return a  <CODE>BigDecimal</CODE> instance
+     */
+    @NotNull
+    StockQuote getQuote(String symbol);
+    
+    /**
+     * Get a historical list of stock quotes for the provided symbol
+     * @param symbol the stock symbol to search for
+     * @param from the date of the first stock quote
+     * @param until the date of the last stock quote
+     * @return a list of StockQuote instances
+     * One for each day in the range specified
+     */
+    
+    @NotNull
+    List<StockQuote> getQuote(String symbol, Calendar from, Calendar until);
+    
+    /**
+     * Get a historical list of stock quotes for the provided symbol
+     * This method will return one StockQuote per interval specified
+     * @param symbol the stock symbol to search for
+     * @param from the date of the first stock quote
+     * @param until the date of the last stock quote
+     * @param interval the number of StockQuotes to get 
+     *  Eg. daily = one StockQuote per day will be returned
+     * @return a list of StockQuote instances
+     * One for each day in the range specified
+     */
+    
+    @NotNull
+    List<StockQuote> getQuote(String symbol, Calendar from, Calendar until, Interval interval);
+
+}
---
+++ src/test/java/edu/KaylaKornelis/advancedjava/AdvancedJavaAssignment4Tests/StockServiceFactoryTest.java
@@ -0,0 +1,25 @@
+package edu.KaylaKornelis.advancedjava.AdvancedJavaAssignment4Tests;
+
+
+import edu.KaylaKornelis.advancedjava.AdvancedJavaAssignment4.service.StockServiceFactory;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+/**
+ * This class tests the public methods found in the 
+ * StockServiceFactory class.
+ */
+public class StockServiceFactoryTest {
+    
+    /** 
+     * This method verifies that a valid Object
+     * is returned by the getStockService method. 
+     */
+    @Test
+    public void stockServiceReturnedNotNull(){
+        //creating new instance of the StockServiceFactory
+        StockServiceFactory factory = new StockServiceFactory();
+        //verify that the object returned by the getStockService method is not null.
+        assertNotNull("StockService returned should not be null", factory.getStockService());
+    }  
+}
