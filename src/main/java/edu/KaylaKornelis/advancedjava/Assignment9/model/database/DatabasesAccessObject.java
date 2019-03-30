e30bf802b4cea4be6707126507bf9eef1dadaf22
Kayla Kornelis <kayla.kornelis@gmail.com>
2019-03-30 00:48:14 -0500

    initial commit optional assignment 9

---
+++ src/main/java/edu/KaylaKornelis/advancedjava/Assignment9/model/database/DatabasesAccessObject.java
@@ -0,0 +1,8 @@
+package edu.KaylaKornelis.advancedjava.Assignment9.model.database;
+
+/**
+ * This is a maker interface which indicates implementers are DAO classes.
+ */
+public interface DatabasesAccessObject{
+
+}
---
+++ src/main/java/edu/KaylaKornelis/advancedjava/Assignment9/services/ServiceFactory.java
@@ -0,0 +1,36 @@
+package edu.KaylaKornelis.advancedjava.Assignment9.services;
+
+/**
+ * A factory that returns a Services.
+ */
+public class ServiceFactory {
+
+    /**
+     * Prevent instantiations
+     */
+    private ServiceFactory() {}
+
+    /**
+     *
+     * @return get a <CODE>StockService</CODE> instance
+     */
+    public static StockService getStockService() {
+        return new DatabaseStockService();
+    }
+
+    /**
+     *
+     * @return get a <CODE>UserService</CODE> instance
+     */
+    public static  UserService getUserService() {
+        return new DatabaseUserService();
+    }
+    
+    /**
+     *
+     * @return get a <CODE>StockService</CODE> instance
+     */
+    public static RestfulStockService getRestfulStockService() {
+        return new RestfulStockService();
+    }
+}
