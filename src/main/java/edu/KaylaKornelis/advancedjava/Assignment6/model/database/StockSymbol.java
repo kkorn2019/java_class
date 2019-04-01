b2cd0b1d9a1a957bc4b3033acdca684dc836fff0
Kayla Kornelis <kayla.kornelis@gmail.com>
2019-04-01 15:08:29 -0500

    adding javadoc to StockSymbol.java per pull request comments

--- src/main/java/edu/KaylaKornelis/advancedjava/Assignment6/model/database/StockSymbol.java
+++ src/main/java/edu/KaylaKornelis/advancedjava/Assignment6/model/database/StockSymbol.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- *
+ * Models the Stock Symbol table
  */
 @Entity
 @Table(name = "stock_symbol", schema = "", catalog = "stocks")
@@ -24,39 +24,71 @@ public class StockSymbol implements DatabasesAccessObject {
         this.symbol = stock.getSymbol();
     }
 
+    /**
+     * Primary Key - Unique ID for a particular row in the stock symbol table.
+     *
+     * @return an integer value
+     */
     @Id
     @Column(name = "id", nullable = false, insertable = true, updatable = true)
     public int getId() {
         return id;
     }
 
+    /**
+     * Set the unique ID for a particular row in the stock symbol table.
+     * This method should not be called by client code. The value is managed internally.
+     *
+     * @param id a unique value.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     *
+     * @return the stock symbol
+     */
     @Basic
     @Column(name = "symbol", nullable = false, insertable = true, updatable = true, length = 4)
     public String getSymbol() {
         return symbol;
     }
 
+    /**
+     * Specify the stock symbol
+     * @param symbol a String value
+     */
     public void setSymbol(String symbol) {
         this.symbol = symbol;
     }
 
+    /**
+     * This method compares two objects to ensure they are equal
+     */
     @Override
     public boolean equals(Object o) {
+        //if the object is equal to itself, return true
         if (this == o) return true;
+        
+        //check if o is null or is not an instance of this class
+        // if so, return false
         if (o == null || getClass() != o.getClass()) return false;
-
+        
+        //cast o to be of type person 
         StockSymbol that = (StockSymbol) o;
 
+        //compare each attribute and return true or false result
         if (id != that.id) return false;
         if (symbol != null ? !symbol.equals(that.symbol) : that.symbol != null) return false;
 
         return true;
     }
 
+    /**
+     * This method constructs and returns a hashcode value for the stock symbol object
+     * @return a hashcode value for the stock symbol object 
+     */
     @Override
     public int hashCode() {
         int result = id;
